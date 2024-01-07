b0b579af6d4dc828c5783ee6f2458426ec2b8a7c
Illya Holubiev <121903042+HolubievIllya@users.noreply.github.com>
2024-01-07 22:56:05 +0200

    Create Exercises.py

---
+++ CaseStudy:HackerStatistics/Exercises.py
@@ -0,0 +1,25 @@
+# TASK 1
+
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
+
+# TASK 2
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
+
+# TASK 3
+
