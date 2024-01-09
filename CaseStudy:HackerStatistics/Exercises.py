55ddde1eb738c45b178da4c11499c047a4df82f9
Illya Holubiev <121903042+HolubievIllya@users.noreply.github.com>
2024-01-09 22:51:30 +0200

    Update Exercises.py

--- CaseStudy:HackerStatistics/Exercises.py
+++ CaseStudy:HackerStatistics/Exercises.py
@@ -23,3 +23,25 @@ print(np.random.randint(1,7))
 
 # TASK 3
 
+# NumPy is imported, seed is set
+import numpy as np
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 4:
+    step = step - 1
+else:
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice, step)
+
+# TASK 4
+
+
